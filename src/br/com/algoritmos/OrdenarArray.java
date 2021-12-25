c206247212859700d017d6875118804ea28c1a02
mariacarolz <mcarolrsouza@gmail.com>
2021-12-25 08:58:16 -0300

    OrdenarArray

--- src/br/com/algoritmos/OrdenarArray.java
+++ src/br/com/algoritmos/OrdenarArray.java
@@ -5,10 +5,44 @@
  */
 package br.com.algoritmos;
 
+import java.util.Arrays;
+
 /**
  *
  * @author maria
  */
 public class OrdenarArray {
-    
+
+    private static int menorValor;
+    private static int menorIndice;
+    private static int[] array;
+
+    public static int buscaMenorValor(int[] array) {
+        menorValor = array[0];
+        menorIndice = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < menorValor) {
+                menorValor = array[i];
+                menorIndice = i;
+            }
+        }
+        return menorIndice;
+    }
+
+    public static int[] ordenacaoPorSelecao(int[] array) {
+        int[] novoArray = new int[0];
+
+        for (int i = 0; i < array.length; i++) {
+            menorValor = buscaMenorValor(array);
+            novoArray[i] = menorValor;
+        }
+        return novoArray;
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
+        System.out.println("O menor indice é " + buscaMenorValor(array));
+        System.out.println("O novo array ordenado é "+  Arrays.toString(ordenacaoPorSelecao(array)));
+    }
 }
