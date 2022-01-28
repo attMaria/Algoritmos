3dbee271bcc8048edce6c18882101ba9dc15ccb6
mariacarolz <mcarolrsouza@gmail.com>
2022-01-28 10:28:45 -0300

    Recursao arrumando

--- src/br/com/algoritmos/algoritmo/Recursao.java
+++ src/br/com/algoritmos/algoritmo/Recursao.java
@@ -5,7 +5,6 @@
  */
 package br.com.algoritmos.algoritmo;
 
-import br.com.algoritmos.entidade.Escola;
 import br.com.algoritmos.util.ArraysNomes;
 import java.util.ArrayList;
 import java.util.List;
@@ -58,32 +57,37 @@ public class Recursao {
 
     public static void main(String[] args) {
         
-        String[] alunos = {"Rafael", "Vitória", "Fabíola", "Bruno", "Jhonata", "Andrey", "Edson", "Gabriel", "Arthur", "Marcelo", "Diego", "Maria",
+        String[] nome = {"Rafael", "Vitória", "Fabíola", "Bruno", "Jhonata", "Andrey", "Edson", "Gabriel", "Arthur", "Marcelo", "Diego", "Maria",
         "Gabriel", "Guilherme", "Eduardo", "Letícia", "Jeferson", "Fábio", "Luccas", "Igor", "Silvio"}; // 21 nomes
-        List<ArrayList> escola = new ArrayList();
+        
+        String[] sobrenome ={"Almeida", "Azevedo", "Braga", "Barros", "Brasil", "Campos", "Cardoso", "Correia"
+                , "Castro", "Costa", "Fontes", "Guimarães" , "Magalhães" , "Macedo", "Matos", "Pedreira", 
+                "Queirós", "Ribeiro", "Rocha", "Siqueira", "Serra", "Souza", "Teixeira", "Valle"};//24 sobrenomes
+             
         qtdeTurmas = 4;
         qtdeGrupos = 2;
         qtdeNomes = 5;
 
-        for (int i = 0; i < qtdeTurmas; i++) {
-            System.out.println("Turma " + (i+1));
-            turma = ArraysNomes.recebeDados(qtdeNomes, qtdeGrupos, alunos);
-            if (!turma.isEmpty()) {
-                escola.add((ArrayList) turma);
-                imprimeArrays(turma);
-            } else {
-                System.out.println("Nomes insuficientes para distribuição igual entre listas!");
-                System.out.println("A lista original de nomes contem " + alunos.length + " items");
-                System.out.println("Para uma ditribuição igual entre grupos, de acordo com os valores passados:");
-                System.out.println("  Nomes por grupo: " + qtdeNomes);
-                System.out.println("  Quantidade de grupos: " + qtdeGrupos);
-                int mais = (qtdeNomes * qtdeGrupos) - alunos.length;
-                System.out.println("o que excede em " + mais + " nomes a lista disponivel.");
-            }
-        }
 
-        String nome = "maria";
-        procWhile(escola, nome);
+//        for (int i = 0; i < qtdeTurmas; i++) {
+//            System.out.println("Turma " + (i+1));
+//            turma = ArraysNomes.recebeDados(qtdeNomes, qtdeGrupos, nome);
+//            if (!turma.isEmpty()) {
+//                escola.add((ArrayList) turma);
+//                imprimeArrays(turma);
+//            } else {
+//                System.out.println("Nomes insuficientes para distribuição igual entre listas!");
+//                System.out.println("A lista original de nomes contem " + nome.length + " items");
+//                System.out.println("Para uma ditribuição igual entre grupos, de acordo com os valores passados:");
+//                System.out.println("  Nomes por grupo: " + qtdeNomes);
+//                System.out.println("  Quantidade de grupos: " + qtdeGrupos);
+//                int mais = (qtdeNomes * qtdeGrupos) - nome.length;
+//                System.out.println("o que excede em " + mais + " nomes a lista disponivel.");
+//            }
+//        }
+//
+//        String nome = "maria";
+//        procWhile(escola, nome);
 
     }
 
